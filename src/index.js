946ec351d447d55f768439f3a7cdbfb796523342
M1n007 <amin4udin@gmail.com>
2018-07-25 18:03:23 +0700

    logic not fixed

--- src/index.js
+++ src/index.js
@@ -7,30 +7,43 @@ export default class DirFinder extends Component{
 
     state = {
         url:'',
-        response:'',
+        result:[],
+        koneksi:[],
+        notfound:[],
+        forbidden:[],
         isLoading:false,
     }
 
     handleScan(){
         let url = this.state.url
-        let dir = ['amin.go.id', 'bismawin', 'kjhjhj']
+        let dir = ['amin.go.id', 'bismawin', 'kjhjhj', 'kaka', 'baaaksakjd', 'dapram_']
         const uri = 'https://'+`${url}`+'/'
-        for (const start = 0; start < dir.length; start++) {
-
-            const urin = uri+`${dir[start]}`
-
+        dir.map(dirin => {
+            const urin = uri+`${dirin}`
             return fetch(urin)
             .then((response) => {
-                if (response.status === 200) {
-                    console.log(urin[start]+"\n")
+                if (response) {
+                    this.setState({
+                        isLoading:true
+                    })
+                    this.setState({
+                        result: response
+                    })
+                    setTimeout(() => {
+                        this.setState({
+                            isLoading:false
+                        })
+                    }, 10000);
                 }
             })
-            .catch((response) =>{
-                alert(response)
-            });
-        }
+        })
     }
     render(){
+        if (this.state.koneksi.length == 0) {
+            null
+        }else{
+            alert(this.state.koneksi)
+        }
         return(
             <Container style={{padding:16,}}>
                     <Form style={{paddingBottom:16}}>
@@ -42,8 +55,23 @@ export default class DirFinder extends Component{
                         <Text style={{color:'white'}}>Scan</Text>
                     </Button>
                     <Form>
-                        <Textarea rowSpan={5} bordered placeholder="Textarea" value=""/>
+                        {
+                            this.state.result.length == 0 ? (
+                                <Textarea bordered value="..."/>
+                            ):
+                            this.state.isLoading == false ? (
+                                <Textarea bordered value="done"/>
+                            ):(
+                                <Textarea bordered value={this.state.result+" "+"scanning...."}/>
+                            )
+                        }
                     </Form>
+                    <Text>Result :</Text>
+                    {
+                        this.state.result.map(results => (
+                            <Text>{result}</Text>
+                        ))
+                    }
             </Container>
         )
     }
