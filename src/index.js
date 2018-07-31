4cff980f69a681c94062470503b1b5ab3529de60
M1n007 <amin4udin@gmail.com>
2018-07-31 21:50:03 +0700

    updated before release

--- src/index.js
+++ src/index.js
@@ -1,78 +1,106 @@
-import React, { Component } from 'react'
-import {View,Text} from 'react-native'
-import {Container, Body, Input, Button, Form, Textarea, Item} from 'native-base'
-import { Col, Row } from 'react-native-easy-grid';
+import React from 'react';
+import { FlatList } from 'react-native';
+import { Button, View, Text, Form, Item, Input, Icon, Spinner,
+Card, CardItem } from 'native-base'
 
-export default class DirFinder extends Component{
+var d = new Date();
+var n = d.getTime();
+var RNFS = require('react-native-fs');
+var path = '/storage/emulated/0/' +n+'_'+'result.txt';
 
-    state = {
-        url:'',
-        result:[],
-        koneksi:[],
-        notfound:[],
-        forbidden:[],
-        isLoading:false,
+export default class DirFinder extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state ={ 
+        isLoading: false, 
+        url: '',
+        dataSource:[],
     }
+  }
 
-    handleScan(){
-        let url = this.state.url
-        let dir = ['amin.go.id', 'bismawin', 'kjhjhj', 'kaka', 'baaaksakjd', 'dapram_']
-        const uri = 'https://'+`${url}`+'/'
-        dir.map(dirin => {
-            const urin = uri+`${dirin}`
-            return fetch(urin)
-            .then((response) => {
-                if (response) {
-                    this.setState({
-                        isLoading:true
-                    })
-                    this.setState({
-                        result: response
-                    })
-                    setTimeout(() => {
-                        this.setState({
-                            isLoading:false
-                        })
-                    }, 10000);
-                }
-            })
+  handleCek(){
+    this.setState({
+      isLoading: true
+    })
+    let url = this.state.url
+    let dir = ['amin.go.id', 'bismawin', 'kjhjhj', 'popo']
+    const uri = 'https://'+`${url}`+'/'
+        dir.map((item)=>{
+
+        return fetch(uri+item)
+        .then((response) => {
+            if (response.status === 200) {
+                this.setState({
+                  dataSource: this.state.dataSource.concat([response.url]),
+                })
+            }else{
+                
+            }})
+
         })
-    }
-    render(){
-        if (this.state.koneksi.length == 0) {
-            null
-        }else{
-            alert(this.state.koneksi)
-        }
-        return(
-            <Container style={{padding:16,}}>
-                    <Form style={{paddingBottom:16}}>
-                        <Item>
-                            <Input placeholder="site.com" onChangeText={(url) => this.setState({url : url})} />
-                        </Item>
-                    </Form>
-                    <Button block primary onPress={() => this.handleScan()}>
-                        <Text style={{color:'white'}}>Scan</Text>
-                    </Button>
-                    <Form>
-                        {
-                            this.state.result.length == 0 ? (
-                                <Textarea bordered value="..."/>
-                            ):
-                            this.state.isLoading == false ? (
-                                <Textarea bordered value="done"/>
-                            ):(
-                                <Textarea bordered value={this.state.result+" "+"scanning...."}/>
-                            )
-                        }
-                    </Form>
-                    <Text>Result :</Text>
-                    {
-                        this.state.result.map(results => (
-                            <Text>{result}</Text>
-                        ))
-                    }
-            </Container>
-        )
-    }
+    this.setState({
+      isLoading: false
+    })
+  }
+
+  toFile(){
+    var uint8array = new TextEncoder("utf-8").encode(this.state.dataSource);
+    var string = new TextDecoder().decode(uint8array);
+    RNFS.writeFile(path, string, 'utf8')
+      .then((success) => {
+        alert('FILE TERSIMPAN di '+path);
+    })
+    .catch((err) => {
+      alert(err.message);
+    });
+  
+  }
+
+  render(){
+    return(
+      <View style={{flex: 1, paddingTop:20}}>
+        <Form style={{paddingBottom:16}}>
+            <Item>
+                <Input placeholder="site.com" onChangeText={(url) => this.setState({url : url})} />
+            </Item>
+        </Form>
+        <Button block primary onPress={() => this.handleCek()}>
+            <Text style={{color:'white'}}>Scan</Text>
+        </Button>
+        <Text>Result :</Text>
+        <Card>
+          <CardItem>
+            {
+              this.state.isLoading == true ? (
+                  <Spinner color='green'/>
+              ):(
+                <View>
+                  <FlatList
+                    data={this.state.dataSource}
+                    renderItem={({item}) => <Text>{item} <Icon style={{color:'green'}} name="md-checkmark" /></Text>}
+                    keyExtractor={(item, index) => index}
+                  />
+                </View>
+              )
+            }
+          </CardItem>
+        </Card>
+
+        <View style={{flex:1, flexDirection:'row', justifyContent:'flex-end', alignSelf:'flex-end'}}>
+          {
+            this.state.dataSource.length == 0 ? (
+              <Button style={{backgroundColor:'grey'}}>
+                <Text style={{color:'white'}}>Save Result</Text>
+              </Button>
+            ):(
+              <Button primary onPress={()=> this.toFile()}>
+                <Text style={{color:'white'}}>Save Result</Text>
+              </Button>
+            )
+          }
+        </View>
+      </View>
+    );
+  }
 }
